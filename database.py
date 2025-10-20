ab482ba38eda59d763581a133e9b7f71aa6295d7
saimali314 <saimali4537@gmail.com>
2025-10-20 08:18:00 +0200

    first commit

--- database.py
+++ database.py
@@ -1,6 +1,14 @@
+# database.py
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-db_url = "postgresql://postgres:aaa11123@localhost:5432/managementsys"
-engine = create_engine(db_url)
-session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# pre_ping helps with serverless DBs waking up
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
